41d5882584ca00ed41973ccf29dd6c966feccde7
kangtoe <kangtoe99@gmail.com>
2025-06-13 00:45:16 +0900

    게임 매니저 스크립트 추가

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+
+    [Header(" Actions ")]
+    public static Action onGamePaused;
+    public static Action onGameResumed;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        // SetGameState(GameState.MENU); // GameState 정의가 없어 주석처리
+    }
+
+    // public void StartGame()             => SetGameState(GameState.GAME); // GameState 정의가 없어 주석처리
+    // public void StartWeaponSelection()  => SetGameState(GameState.WEAPONSELECTION); // GameState 정의가 없어 주석처리
+    // public void StartShop()             => SetGameState(GameState.SHOP); // GameState 정의가 없어 주석처리
+
+    // public void SetGameState(GameState gameState)
+    // {
+    //     IEnumerable<IGameStateListener> gameStateListeners = 
+    //         FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None)
+    //         .OfType<IGameStateListener>();
+
+    //     foreach (IGameStateListener gameStateListener in gameStateListeners)
+    //         gameStateListener.GameStateChangedCallback(gameState);
+    // }
+
+    public void WaveCompletedCallback()
+    {
+        // if(Player.instance.HasLeveledUp() || WaveTransitionManager.instance.HasCollectedChest())
+        // {
+        //     SetGameState(GameState.WAVETRANSITION);
+        // }
+        // else
+        // {
+        //     SetGameState(GameState.SHOP);
+        // }
+    }
+
+    public void ManageGameover()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PauseButtonCallback()
+    {
+        Time.timeScale = 0;
+        onGamePaused?.Invoke();
+    }
+
+    public void ResumeButtonCallback()
+    {
+        Time.timeScale = 1;
+        onGameResumed?.Invoke();
+    }
+
+    public void RestartFromPause()
+    {
+        Time.timeScale = 1;
+        ManageGameover();
+    }
+}
+
+// public interface IGameStateListener
+// {
+//     void GameStateChangedCallback(GameState gameState);
+// }
