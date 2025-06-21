3eaa450815388a75d88af24f586a62b8749cbfaa
kangtoe <kangtoe99@gmail.com>
2025-06-22 02:24:43 +0900

    웨이브 종료 시, 스폰 대기 오브젝트도 취소되도록 함

--- Assets/Scripts/Spawn/SpawnIndicator.cs
+++ Assets/Scripts/Spawn/SpawnIndicator.cs
@@ -11,23 +11,33 @@ public class SpawnIndicator : MonoBehaviour
     {
         Invoke(nameof(TrySpawn), spawnDelay);
     }
-    
+
+    void Update()
+    {
+        if (!WaveSpawnManager.Instance.CanSpawn)
+        {
+            CancelInvoke(nameof(TrySpawn));
+            Destroy(gameObject);
+        }
+    }
+
     public void Init(EnemyBase _spawnPrefab, float _spawnDelay = 3f)
     {
         spawnPrefab = _spawnPrefab;
         spawnDelay = _spawnDelay;
-        
+
     }
 
     void TrySpawn()
     {
         Destroy(gameObject);
-        
+
         // should check game state
         if (spawnPrefab != null)
         {
-            EnemyBase spawnObject = Instantiate(spawnPrefab, transform.position, Quaternion.identity);      
-            EnemySpawner.Instance.RegisterEnemy(spawnObject);  
-        }        
+            EnemyBase spawnObject = Instantiate(spawnPrefab, transform.position, Quaternion.identity);
+            spawnObject.transform.SetParent(WaveSpawnManager.Instance.transform);
+            WaveSpawnManager.Instance.RegisterEnemy(spawnObject);
+        }
     }
 }
